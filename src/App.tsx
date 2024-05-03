4f5a5b99171677fb291ee70b1e4ca43630799c15
Esma Dervisevic <esma.d@ecronix.com>
2024-05-03 18:19:37 +0200

    using context

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
 import Sidebar from "./components/Sidebar/Sidebar";
-
+import { useMovieDBContext } from "./context/movieDBContext";
 function App() {
+  const movieContext = useMovieDBContext();
+
+  const { trendingMovies } = movieContext;
+
+  console.log(trendingMovies);
+
   return (
     <div className="app">
       <Sidebar />
